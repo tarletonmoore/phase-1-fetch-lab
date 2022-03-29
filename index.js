1b7e7aed47c04568edad31fd1f93364d2fa13a04
John Tarleton Moore <tarletonmoore@gmail.com>
2022-03-29 14:03:15 -0400

    commit tree 986ed3e0bb034318faa879c8c469ae6b74b8ca5a

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then(response => response.json())
+    .then(response => renderBooks(response))
+
 }
 
 function renderBooks(books) {
@@ -12,6 +15,6 @@ function renderBooks(books) {
   });
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   fetchBooks();
 });
